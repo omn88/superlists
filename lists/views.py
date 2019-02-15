d05fffc65a03c178a11558f671e63e63f24bc250
Mikolaj <mikonizio@gmail.com>
2019-02-15 16:38:10 +0100

    First unit test and url mapping, dummy view

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+
+def home_page():
+	pass
