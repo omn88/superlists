f2931547984a2e499e9e817e80fe615c0bd66dea
Mikolaj <mikonizio@gmail.com>
2019-02-22 16:40:52 +0100

    List Model and relation to Item model created

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class List(models.Model):
+	text = models.TextField(default='')
+	list_name = models.TextField(default='')
+	
+	def __str__(self):
+		return self.list_name
+	
 class Item(models.Model):
 	text = models.TextField(default='')
+	list = models.ForeignKey(List, default = None)
+	
+	def __str__(self):
+		return self.text
