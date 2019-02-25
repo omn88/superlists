33542a788d774a8fdc5808d2ad7b71ddb87fd466
Mikolaj <mikonizio@gmail.com>
2019-02-25 13:11:20 +0100

    Chapter finished, unit tests and functional tests ok

--- lists/urls.py
+++ lists/urls.py
@@ -5,8 +5,7 @@ from lists import views
 app_name = 'lists'
 urlpatterns = [
 
-    url(r'^$', views.home_page, name='home_page'),
-	url(r'^lists/new$', views.new_list, name='new_list'),
-	url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
-	url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+	url(r'^new$', views.new_list, name='new_list'),
+	url(r'^(\d+)/$', views.view_list, name='view_list'),
+	url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lists import views
+from lists import views as list_views
+from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('lists.urls')),
+	url(r'^$', list_views.home_page, name='home_page'),
+    url(r'^lists/', include('lists.urls')),
 ]
