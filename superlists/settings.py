98c6ec32b6ebb590427c378bf9143ff03ff54d58
Mikolaj <mikonizio@gmail.com>
2019-03-04 19:27:42 +0100

    Settings changed

--- superlists/settings.py
+++ superlists/settings.py
@@ -20,6 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 if 'DJANGO_DEBUG_FALSE' in os.environ:
 	DEBUG = False
 	SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
